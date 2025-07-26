603932c53d9f9b5281d536ae515c1084555c6e0a
Choto02 <62578702+Choto02@users.noreply.github.com>
2025-07-26 15:53:09 -0400

    Add files via upload

---
+++ Presim/INTT/run_presim.f
@@ -0,0 +1,13 @@
+//tb
+/home/sparclab/a/sayeeda/04_Dilithium_TSMC_0425/Presim/INTT/INTT_tb.v
+
+//NTT
+/home/sparclab/a/sayeeda/04_Dilithium_TSMC_0425/RTL/INTT_FSM.v
+// /home/sparclab/a/sayeeda/04_Dilithium_TSMC_0425/RTL/NTT_FSM.v
+/home/sparclab/a/sayeeda/04_Dilithium_TSMC_0425/RTL/INTT_top.v
+
+//Reduction
+/home/sparclab/a/sayeeda/04_Dilithium_TSMC_0425/RTL/DRed.v
+
+//Mem 2 port
+/home/sparclab/a/sayeeda/04_Dilithium_TSMC_0425/RTL/mem_2port.v
